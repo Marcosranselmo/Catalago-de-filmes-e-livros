729d51d850533c2d3c5ce67920377b7d922bb9c2
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-01-31 11:50:17 -0300

    delete-category

--- administracao/categoriasAdm.php
+++ administracao/categoriasAdm.php
@@ -26,9 +26,9 @@
     <!-- PRINCIPAL -->
 
     <main class="container">
-        <h3 class="text-center mt-5 mb-4">Categorias - Administração</h3><br>
+        <h3 class="text-center mt-5">Categorias - Administração</h3><br>
         <div class="row-auto d-flex">
-            <div class="col-md-3 col-sm-3">
+            <div class="col-md-3 col-sm-2 mx-3">
                 <?php include_once "menuAdm.html" ?>            
             </div>
             <div class="col-md-9 col-sm-9">
@@ -36,29 +36,45 @@
                     if(isset($_GET['btnSubmitCategoria'])) {
                         $nomeCategoria = $_GET['txtCategoria'];
                         $link = $nomeCategoria;
-                        $sql = "CALL sp_cadastra_categoria('$nomeCategoria','$link',@saida);";
+                        $sql = "CALL sp_cadastra_categoria('$nomeCategoria','$link',@saida, @rotulo);";
                         if($res=mysqli_query($con,$sql)) {
                             $reg=mysqli_fetch_assoc($res);
                             $saida = $reg['saida'];
-                            echo $saida;
-                            echo "<br><br>";
-                            echo "<a href='categoriasAdm.php' target='_self'>Voltar</a> ";
+                            $rotulo = $reg['saida_rotulo'];
+                            switch ($rotulo) {
+                                case 'TUDO CERTO!':
+                                    $alert = 'alert-success';
+                                    break;
+                                case 'OPS!':
+                                    $alert = 'alert-warning';
+                                    break;  
+                                case 'ERRO!':
+                                    $alert = 'alert-danger';
+                                    break;      
+                            }
+                            ?>
+                            <div class="alert <?php echo $alert; ?>" role="alert">
+                                <h3><?php echo $rotulo; ?></h3>
+                                <?php echo $saida; ?>
+                                <a href="categoriasAdm.php" class="alert-link" target="_self">Voltar</a>
+                            </div>
+                            <?php
                         } else {
                             echo "Erro ao executar a query.";
                         }
                     }else{
                     
                 ?>
-                <h3 class="text-center mb-4">Cadastro Categorias</h3>
+                <h4 class="text-center mb-4">Cadastro Categorias</h4>
                 <form name="fmCategorias" method="get" action="categoriasAdm.php" onsubmit="return validaCampos()">
-                    </label>Nome da categoria</label><br>
-                    <input type="text" name="txtCategoria" class="form-control" maxlength="30">
-                    <button type="submit" class="btn btn-primary w-100 mt-2" name="btnSubmitCategoria">Cadastrar</button>
+                    <label>Nome da categoria</label><br>
+                    <input type="text" name="txtCategoria" class="form-control my-3" maxlength="50">
+                    <button type="submit" class="btn btn-primary w-100" name="btnSubmitCategoria">Cadastrar</button>
                 </form>     
                 <br>
-                <hr>
-                <h2 class="text-center mb-4">Categorias cadastradas:</h2>
-                <div class="row">
+                <hr/>
+                <h2 class="text-center mb-3">Categorias cadastradas:</h2>
+                <div class="row mx-1">
                     <?php  
                         $sql = 'SELECT * FROM vw_retorna_categorias';
                         if ($res=mysqli_query($con, $sql)) {
@@ -73,14 +89,15 @@
                                 ?>
 
                                     <div class="col-md-3 itensCadastrados text-center">
-                                        <label><?php echo $nomeCategoria[$i]; ?></label>
+                                        <label class="col-md-12"><?php echo $nomeCategoria[$i]; ?></label>
                                         <div class="btn-group btn-group-sm" role="group" arial-label="Basic sample">
                                             <a href="editaCategoriaAdm.php?editaCategoria=<?php echo $codigoCategoria[$i];
                                             ?>" class="btn btn-primary">Editar</a>
-                                            <a href="categoriasAdm.php?excluirCategoria=<?php echo $codigoCategoria[$i]; 
+                                            <a href="editaCategoriaAdm.php?excluirCategoria=<?php echo $codigoCategoria[$i]; 
                                             ?>" class="btn btn-secondary" onclick="return confirm('Tem certeza que deseja excluir esta categoria?')">Excluir</a>
                                         </div>
                                     </div>
+                                    
                             <?php
                             $i++;
                             }
---
+++ administracao/editaCategoriaAdm.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php
+        include_once "header.html";
+        include_once "../mais/conexao.php";
+    ?>
+    <title>Home</title>
+    <script type="text/javascript">
+        function validaCampos() {
+            if (document.fmCategorias.txtCategoria.value == "" ) {
+                alert("Favor preencher no campo o nome da categoria");
+                document.fmCategorias.txtCategoria.focus();
+                return false;
+            }
+        }
+    </script>
+</head>
+<body class="administracao">
+
+    <!-- MENU SUPERIOR -->
+
+    <?php include_once "menuSuperior.html"; ?>
+    <!-- FIM MENU SUPERIOR -->
+
+    <!-- PRINCIPAL -->
+
+    <main class="container">
+        <h3 class="text-center mt-5">Categorias - Administração</h3><br>
+        <div class="row-auto d-flex">
+            <div class="col-md-3 col-sm-2 mx-3">
+                <?php include_once "menuAdm.html" ?>            
+            </div>
+            <div class="col-md-9 col-sm-9">
+                <?php 
+                    if(isset($_GET['excluirCategoria'])) {
+                        $codigoCategoria = $_GET['excluirCategoria'];
+                        $sql = "CALL sp_deleta_categoria('$codigoCategoria', @saida, @rotulo);";
+                        if ($res=mysqli_query($con, $sql)) {
+                            $reg=mysqli_fetch_assoc($res);
+                            $saida = $reg['saida'];
+                            $rotulo = $reg['saida_rotulo'];
+                            switch ($rotulo) {
+                                case 'TUDO CERTO!':
+                                    $alert = 'alert-success';
+                                    break;
+                                case 'OPS!':
+                                    $alert = 'alert-warning';
+                                    break;  
+                                case 'ERRO!':
+                                    $alert = 'alert-danger';
+                                    break;      
+                            }
+                            ?>
+                            <div class="alert <?php echo $alert; ?>" role="alert">
+                                <h3><?php echo $rotulo; ?></h3>
+                                <?php echo $saida; ?>
+                                <a href="categoriasAdm.php" class="alert-link" target="_self">Voltar</a>
+                            </div>
+                            <?php
+                        } else {
+                            echo "Erro ao executar a query.";
+                        }
+                    }else{
+                    }
+                ?>
+            </div>
+        </div>
+    </main>
+    <?php if(isset($con)){ mysqli_close($con); } ?>
+</body>
+</html>
