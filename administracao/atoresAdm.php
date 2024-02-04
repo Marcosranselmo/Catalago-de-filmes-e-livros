b8b43e6eac679f7cae1dd14ef4f85c20730aec4e
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-02-03 22:11:13 -0300

    edit-atores-imagens

--- administracao/atoresAdm.php
+++ administracao/atoresAdm.php
@@ -81,20 +81,67 @@ if ($_SESSION['acesso'] == true) {
                         $sql = "CALL sp_cadastra_atores('$nome','$pais','$bio','$nomeImagem1','$nomeImagem2','$nomeImagem3','@saida','@saida_rotulo')";
 
                         executaQuery($sql, "atoresAdm.php");
+
                     } else {
                     ?>
 
                         <ul class="nav nav-tabs" role="tablist">
+
                             <li class="nav-item" role="presentation">
-                                <a href="#tabFormulario" class="nav-link active" id="linkFormulario" data-toggle="tab" role="tab" aria-controls="tabFormulario">Cadastro</a>
+                                <a href="#tabExibicao" class="nav-link active" id="linkExibicao" data-toggle="tab" role="tab" aria-controls="tabExibicao">Atores/Atrizes Cadastrados</a>
                             </li>
+
                             <li class="nav-item" role="presentation">
-                                <a href="#tabExibicao" class="nav-link" id="linkExibicao" data-toggle="tab" role="tab" aria-controls="tabExibicao">Atores/Atrizes Cadastrados</a>
+                                <a href="#tabFormulario" class="nav-link" id="linkFormulario" data-toggle="tab" role="tab" aria-controls="tabFormulario">Cadastro</a>
                             </li>
+
                         </ul>
 
                         <div class="tab-content" id="meusConteudos">
-                            <div class="tab-pane fade show-active" id="tabFormulario" role="tabpanel" aria-labelledby="linkFormulario">
+
+                            <div class="tab-pane fade show-active mt-5 p-3" id="tabExibicao" role="tabpanel" aria-labelledby="linkExibicao">
+                                <h4 class="text-center mb-4">Atores Cadastrados</h4>
+                                <div class="row">
+                                    <?php
+                                    $sql = "SELECT * FROM vw_retorna_atores";
+                                    if ($res = mysqli_query($con, $sql)) {
+
+                                        $nomeAtor = array();
+                                        $codigoAtor = array();
+                                        $imagemAtor = array();
+                                        $i = 0;
+
+                                        while ($reg = mysqli_fetch_assoc($res)) {
+                                        $nomeAtor[$i] = $reg['nome_ator'];
+                                        $codigoAtor[$i] = $reg['codigo_ator'];
+                                        $imagemAtor[$i] = $reg['caminho_imagem'];
+
+                                        if (!isset($imagemAtor[$i])) {
+                                            $imagemAtor[$i] = "sem_imagem.jpg";
+                                        }
+                                        ?>
+                                        <div class="col-md-4 itensCadastrados text-center">
+                                            <h6><?php echo $nomeAtor[$i]; ?></h6>
+                                            <img src="../imagens/atores/<?php echo $imagemAtor[$i]; ?>" class="img-responsive img-thumbnail mb-3" alt="">
+                                            <!-- LINHA INATIVA <h6><?php echo $nomeAtor[$i] . " codigo: " . $codigoAtor[$i]; ?></h6> -->
+                                            <div class="btn-group btn-group-sm" role="group" arial-label="Basic sample">
+                                                <a href="editaAtorAdm.php?editaAtor=<?php echo $codigoAtor[$i]; ?>&nomeAtor=<?php echo 
+                                                    $nomeAtor[$i]; ?>" class="btn btn-primary">Editar</a>
+                                                <a href="editaAtorAdm.php?excluirAtor=<?php echo $codigoAtor[$i]; ?>" class="btn btn-secondary" 
+                                                    onclick="return confirm('Tem certeza que deseja excluir este(a) ator(a)?')">Excluir</a>
+                                            </div>
+                                        </div>
+
+                                        <?php
+                                        $i++;
+                                        }
+                                    }
+
+                                    ?>
+                                </div>
+                            </div>
+
+                            <div class="tab-pane fade" id="tabFormulario" role="tabpanel" aria-labelledby="linkFormulario">
 
                                 <h4 class="text-center mt-5 mb-4">Cadastrar Novos Atores</h4>
                                 <form name="fmAtores" method="post" action="atoresAdm.php" enctype="multipart/form-data" onsubmit="return validaCampos()">
@@ -136,48 +183,6 @@ if ($_SESSION['acesso'] == true) {
 
                             </div>
 
-                            <div class="tab-pane fade mt-5" id="tabExibicao" role="tabpanel" aria-labelledby="linkExibicao">
-                                <h4 class="text-center">Atores Cadastrados</h4>
-                                <div class="row">
-                                    <?php
-                                    $sql = "SELECT * FROM vw_retorna_atores";
-                                    if ($res = mysqli_query($con, $sql)) {
-
-                                        $nomeAtor = array();
-                                        $codigoAtor = array();
-                                        $imagemAtor = array();
-                                        $i = 0;
-
-                                        while ($reg = mysqli_fetch_assoc($res)) {
-                                        $nomeAtor[$i] = $reg['nome_ator'];
-                                        $codigoAtor[$i] = $reg['codigo_ator'];
-                                        $imagemAtor[$i] = $reg['caminho_imagem'];
-
-                                        if (!isset($imagemAtor[$i])) {
-                                            $imagemAtor[$i] = "sem_imagem.jpg";
-                                        }
-                                        ?>
-                                        <div class="col-md-4 itensCadastrados text-center">
-                                            <h6><?php echo $nomeAtor[$i]; ?></h6>
-                                            <img src="../imagens/atores/<?php echo $imagemAtor[$i]; ?>" class="img-responsive img-thumbnail mb-3" alt="">
-                                            <!-- LINHA INATIVA <h6><?php echo $nomeAtor[$i] . " codigo: " . $codigoAtor[$i]; ?></h6> -->
-                                            <div class="btn-group btn-group-sm" role="group" arial-label="Basic sample">
-                                                <a href="editaAtorAdm.php?editaAtor=<?php echo $codigoAtor[$i]; ?>&nomeAtor=<?php echo 
-                                                    $nomeAtor[$i]; ?>" class="btn btn-primary">Editar</a>
-                                                <a href="editaAtorAdm.php?excluirAtor=<?php echo $codigoAtor[$i]; ?>" class="btn btn-secondary" 
-                                                    onclick="return confirm('Tem certeza que deseja excluir este(a) ator(a)?')">Excluir</a>
-                                            </div>
-                                        </div>
-
-                                        <?php
-                                        $i++;
-                                        }
-                                    }
-
-                                    ?>
-                                </div>
-                            </div>
-
                         </div>
                     <?php
                     }
@@ -191,10 +196,9 @@ if ($_SESSION['acesso'] == true) {
         } ?>
     </body>
 
-    <script src="https://kit.fontawesome.com/4bb29d1df9.js" crossorigin="anonymous"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
-    </script>
+<script src="https://kit.fontawesome.com/4bb29d1df9.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+</script>
 
 <?php
 } else { ?>
--- administracao/editaAtorAdm.php
+++ administracao/editaAtorAdm.php
@@ -57,10 +57,87 @@ if ($_SESSION['acesso'] == true) {
                             $sql = "CALL sp_deleta_atores($codigoAtor, @saida, @saida_rotulo)";
                             executaQuery($sql, "atoresAdm.php");
 
-                        }else{
+                        }elseif(isset($_GET['editaAtor'])) {
 
+                            /* CRIAÇÃO DE ARRAYS DE SESSÃO */
+                            $_SESSION['caminho_imagem'] = array();
+                            $_SESSION['codigo_imagem'] = array();
+    
+                            /* CARREGAR AS INFORMAÇÕES DO(A) ATOR/ATRI */
+                            $codigoAtor = $_GET['editaAtor'];
+                            $_SESSION['codigo_ator'] = $codigoAtor;
+    
+                            $sql = "SELECT * FROM vw_retorna_atores WHERE codigo_ator = $codigoAtor";
+                            if ($res = mysqli_query($con, $sql)) {
+                                $reg = mysqli_fetch_assoc($res);
+                                $nomeAtor = $reg['nome_ator'];
+                                $paisAtor = $reg['pais_ator'];
+                                $biografiaAtor = $reg['biografia_ator'];
+                            }else{
+                                echo "Algo deu errado ao executar a query!";
+                            }
+
+                            $imagnsAtor = array();
+                            $imagensCodigo = array();
+                            $i = 0;
+                            $sql = "SELECT * FROM imagens WHERE atores_codigo = $codigoAtor";
+                            if ($res = mysqli_query($con, $sql)) {
+                                while ($reg = mysqli_fetch_assoc($res)) {
+                                    $imagensAtor[$i] = $reg['caminho'];
+                                    $imagensCodigo[$i] = $reg['codigo'];
+                                    $i++;
+                                }
+                            }else{
+                                echo "Algo deu errado ao executar a query!";
+                            } ?>
+
+                            <!-- EXIBIR INFORMÇÕES DO ATRO/ATRIZ NO FORMULÁRIO -->
+                            <form name="fmAtores" method="post" action="editaAtorAdm.php" enctype="multipart/form-data" onsubmit="return validaCampos()">
+                                <label>Nome:</label>
+                                <input type="text" name="txtNome" class="form-control mb-2" maxlength="70" value="<?php echo $nomeAtor; ?>">
+                                <label>Páis:</label>
+                                <select name="selPais" class="form-control mb-2">
+                                    <option value="0">Selecione o País</option>
+                                    <?php
+                                    $sql = "SELECT * FROM vw_retorna_pais";
+                                    if ($res = mysqli_query($con, $sql)) {
+                                        $nomePais = array();
+                                        $codigoPais = array();
+                                        $i = 0;
+                                        while ($reg = mysqli_fetch_assoc($res)) {
+                                            $nomePais[$i] = $reg['nome_pais'];
+                                            $codigoPais[$i] = $reg['codigo_pais'];
+                                            ?>
+                                            <option value="<?php echo $codigoPais[$i]; ?>" <?php if ($codigoPais[$i] == $paisAtor)
+                                            {
+                                                echo "selected";
+                                            } ?> ><?php echo $nomePais[$i]; ?></option>
+                                    <?php
+                                            $i++;
+                                        }
+                                    }
+                                    ?>
+                                </select>
+
+                                <label>Biografia do Ator:</label>
+                                <textarea name="txtBiografia" maxlength="500" placeholder="Digite descrição aqui" cols="87" rows="4" class="form-control mb-2">
+                                    <?php echo $biografiaAtor; ?>
+                                </textarea>
+
+                                <label>Fotos do Ator</label>
+                                <input type="file" name="fileImagemAtor1" class="btn btn-success w-100 mb-2" accept="image/png, image/jpg">
+                                <input type="file" name="fileImagemAtor2" class="btn btn-success w-100 mb-2" accept="image/png, image/jpg">
+                                <input type="file" name="fileImagemAtor3" class="btn btn-success w-100 mb-2" accept="image/png, image/jpg">
+
+                                <button type="submit" name="btnSubmitAtores" class="btn btn-primary w-100">Cadastrar</button>
+                                <br><br>
+                            </form>
+
+                            <?php
+                        }else{
+    
                         }
-                    ?>
+                        ?>
                 </div>
             </div>
         </main>
@@ -70,9 +147,9 @@ if ($_SESSION['acesso'] == true) {
         } ?>
     </body>
 
-    <script src="https://kit.fontawesome.com/4bb29d1df9.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
-    </script>
+<script src="https://kit.fontawesome.com/4bb29d1df9.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+</script>
 
 <?php
 } else { ?>
